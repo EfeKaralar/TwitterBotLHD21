314dcd9365e5c26a79eb65bbc39cd5190780f9d7
Alp Efe Karalar <59177243+EfeKaralar@users.noreply.github.com>
2021-03-29 13:37:58 +0300

    Add files via upload

---
+++ credentials.py
@@ -0,0 +1,12 @@
+# Credentials for your Twitter bot account
+
+# 1. Sign into Twitter or create new account
+# 2. Make sure your mobile number is listed at twitter.com/settings/devices
+# 3. Head to apps.twitter.com and select Keys and Access Tokens
+
+CONSUMER_KEY = 'ow3DnmrE8zvTjU5ILrb4wNt8y'
+CONSUMER_SECRET = '7l5I3bSyCF6LLcxqY80YL8btFftdMNkKIWpgDBEZeiOQ8Kc3Fs'
+
+# Create a new Access Token
+ACCESS_TOKEN = '1126080248951517184-sctbikUFA6DoGWrKAyuvXRRHq4676a' 
+ACCESS_SECRET = 'baJ5BnDbyR5huTHr4hzBWxiRvaB0nb2zTL5o8pzEsgx9c'
