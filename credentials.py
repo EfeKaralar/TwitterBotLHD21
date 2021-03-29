bb5687db9ad2437a17307ff11b13d67197c84c1e
Alp Efe Karalar <59177243+EfeKaralar@users.noreply.github.com>
2021-03-29 13:38:30 +0300

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -4,9 +4,9 @@
 # 2. Make sure your mobile number is listed at twitter.com/settings/devices
 # 3. Head to apps.twitter.com and select Keys and Access Tokens
 
-CONSUMER_KEY = 'ow3DnmrE8zvTjU5ILrb4wNt8y'
-CONSUMER_SECRET = '7l5I3bSyCF6LLcxqY80YL8btFftdMNkKIWpgDBEZeiOQ8Kc3Fs'
+CONSUMER_KEY = 'XXXXXX'
+CONSUMER_SECRET = 'XXXXXXXXX'
 
 # Create a new Access Token
-ACCESS_TOKEN = '1126080248951517184-sctbikUFA6DoGWrKAyuvXRRHq4676a' 
-ACCESS_SECRET = 'baJ5BnDbyR5huTHr4hzBWxiRvaB0nb2zTL5o8pzEsgx9c'
+ACCESS_TOKEN = 'XXXXXX' 
+ACCESS_SECRET = 'XXXXXXXX'
